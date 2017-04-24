4a1684bba66bafcdaa073429bc979b99c86337ff
Heiko Braun <ike.braun@googlemail.com>
2017-04-24 10:07:48 +0200

    First API files

---
+++ api/src/main/java/org/eclipse/microprofile/health/HealthStatus.java
@@ -0,0 +1,8 @@
+package org.eclipse.microprofile.health;
+
+/**
+ * @author Heiko Braun
+ * @since 24.04.17
+ */
+public class HealthStatus {
+}
---
+++ api/src/main/java/org/eclipse/microprofile/health/inject/Health.java
@@ -0,0 +1,8 @@
+package org.eclipse.microprofile.health.inject;
+
+/**
+ * @author Heiko Braun
+ * @since 24.04.17
+ */
+public class Health {
+}
